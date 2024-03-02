7ee4c9f39a06773ab14b10310dbce06fa2a9fd74
Madison Funderburk <mfunderburk@teleflora.com>
2024-03-02 16:09:48 -0600

    Initial Commit

---
+++ checklistr/checklistrApp.swift
@@ -0,0 +1,17 @@
+//
+//  checklistrApp.swift
+//  checklistr
+//
+//  Created by Madison Funderburk on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct checklistrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
